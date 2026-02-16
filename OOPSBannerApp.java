79251885c0c67273232bf2782785a05ba88a5bcf
Tanisha-Harish <tanishahari08@gmail.com>
2026-02-16 15:12:30 +0530

    Coded UC2 to Print OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,22 @@
 /**
  * OOPSBannerApp
+ * UC2 - Print OOPS as Banner
  * @author Tanisha
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        System.out.println(" *****    *****    ******   ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *         ***** ");
+
+    }
 }
+
